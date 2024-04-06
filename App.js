00de5e3b523fe160ebc7288ff3750ad5a8983169
biel-correa <gabriel@correa.dev.br>
2024-04-06 10:29:27 -0300

    feature: create MapPage

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import WelcomePage from './pages/WelcomePage';
 import HomePage from './pages/HomePage';
+import MapPage from './pages/MapPage';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,7 @@ export default function App() {
       <Stack.Navigator initialRouteName='Welcome'>
         <Stack.Screen name="Welcome" component={WelcomePage} />
         <Stack.Screen name="Home" component={HomePage} />
+        <Stack.Screen name="Map" component={MapPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ pages/MapPage.js
@@ -0,0 +1,11 @@
+import { Text } from 'react-native';
+import BasePage from '../components/BasePage';
+
+export default function MapPage() {
+
+  return (
+    <BasePage>
+      <Text>Map Page</Text>
+    </BasePage>
+  );
+}
