89540fba0e1e0e9134baab5cbdc1dfec7221e847
biel-correa <gabriel@correa.dev.br>
2024-04-06 10:03:26 -0300

    feature: configure routes

---
+++ pages/HomePage.js
@@ -0,0 +1,11 @@
+import { Text } from 'react-native';
+import BasePage from '../components/BasePage';
+
+export default function HomePage() {
+
+  return (
+    <BasePage>
+      <Text>Home Page</Text>
+    </BasePage>
+  );
+}
---
+++ pages/WelcomePage.js
@@ -0,0 +1,11 @@
+import { Text } from 'react-native';
+import BasePage from '../components/BasePage';
+
+export default function WelcomePage() {
+
+  return (
+    <BasePage>
+      <Text>Welcome Page</Text>
+    </BasePage>
+  );
+}
