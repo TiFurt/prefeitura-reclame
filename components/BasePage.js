cf6cdcd84be080baaf295713352f7bd9e8d80fa2
TiFurt <tiagofurtadopfcarneiro@gmail.com>
2024-04-07 16:46:09 -0300

    fix: wrong padding top of screen

--- components/BasePage.js
+++ components/BasePage.js
@@ -1,13 +1,10 @@
 import { View, StyleSheet } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function BasePage({ children }) {
   return (
-    <SafeAreaView>
       <View style={style.container}>
         {children}
       </View>
-    </SafeAreaView>
   )
 }
 
