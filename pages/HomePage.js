c185ae014d638d15d1ec4bfa90bf8d120baa8fc0
TiFurt <tiagofurtadopfcarneiro@gmail.com>
2024-06-10 20:16:15 -0300

    temp

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,15 +1,20 @@
 import { AntDesign } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { useIsFocused } from '@react-navigation/native';
+import { authenticateAsync, hasHardwareAsync } from "expo-local-authentication";
 import { useEffect, useState } from "react";
-import { ScrollView, StyleSheet } from "react-native";
+import { ScrollView, StyleSheet, Text, TouchableOpacity } from "react-native";
 import BasePage from "../components/BasePage";
 import CardComponent from "../components/CardComponent";
 import FloatingActionComponent from "../components/FloatingActionComponent";
 import { routes } from "../routes";
 import ClaimService from '../services/ClaimService';
-import { useIsFocused } from '@react-navigation/native';
+import AuthService from '../services/AuthService';
 
 export default function HomePage({ navigation }) {
   const [claims, setClaims] = useState([]);
+  const [biometry, setBiometry] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const isFocused = useIsFocused();
 
   const _listClaims = () => {
@@ -37,6 +42,61 @@ export default function HomePage({ navigation }) {
     }
   }, [isFocused]);
 
+  const _authenticateBiometryAsync = async () => {
+    const result = await authenticateAsync();
+    if (!result.success) {
+      return;
+    }
+
+    setIsAuthenticated(true);
+  };
+
+  const _hasBiometryHardware = async () => {
+    const valid = await hasHardwareAsync();
+    setBiometry(valid);
+
+    if (valid) {
+      await _authenticateBiometryAsync();
+    }
+  };
+
+  useEffect(() => {
+    if (AuthService.getInstance().recentAuthenticated) {
+      setBiometry(true);
+      setIsAuthenticated(true);
+      return;
+    }
+
+    (async () => await _hasBiometryHardware())();
+  }, []);
+
+  if (!biometry) {
+    return (
+      <BasePage>
+        <Text>Seu dispositivo não tem suporte para biometria.</Text>
+      </BasePage>
+    );
+  }
+
+  if (!isAuthenticated) {
+    return (
+      <BasePage>
+        <Text>Autentique-se para continuar.</Text>
+
+        <TouchableOpacity
+          onPress={(async () => await _authenticateBiometryAsync())}
+          style={styles.submit}
+          title="Entrar"
+          accessibilityLabel="Entrar"
+        >
+          <Text style={styles.submitLabel}>Autenticar com biometria</Text>
+
+          <FontAwesome5 name="fingerprint" size={20} color="white" />
+        </TouchableOpacity>
+      </BasePage>
+    );
+  }
+
   return (
     <BasePage>
       <ScrollView contentContainerStyle={styles.scrollView}>
@@ -60,5 +120,20 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     gap: 10,
     padding: 10,
+  },
+  submit: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1253bc",
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 10,
+  },
+  submitLabel: {
+    color: "white",
+    fontWeight: "bold",
   },
 });
