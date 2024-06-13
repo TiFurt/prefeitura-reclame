5c3c1c09a384905ec0ed407ae4ae7c140985dddd
biel-correa <gabriel@correa.dev.br>
2024-06-13 19:40:52 -0300

    style: improve welcome buttons

--- pages/WelcomePage.js
+++ pages/WelcomePage.js
@@ -1,4 +1,5 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity, StyleSheet, Text, View } from 'react-native';
+import { FontAwesome5 } from "@expo/vector-icons";
 import BasePage from '../components/BasePage';
 import { routes } from '../routes';
 
@@ -15,8 +16,27 @@ export default function WelcomePage({ navigation }) {
   return (
     <BasePage>
       <View style={styles.container}>
-        <Button title='Login' onPress={_goToLogin}></Button>
-        <Button title='Criar Conta' onPress={_goToRegister}></Button>
+        <TouchableOpacity
+          onPress={_goToLogin}
+          style={styles.button}
+          title="Login"
+          accessibilityLabel="Login"
+        >
+          <Text style={styles.buttonLabel}>Login</Text>
+
+          <FontAwesome5 name="sign-in-alt" size={20} color="white" />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={_goToRegister}
+          style={styles.button}
+          title="Criar Conta"
+          accessibilityLabel="Criar Conta"
+        >
+          <Text style={styles.buttonLabel}>Criar Conta</Text>
+
+          <FontAwesome5 name="sign-in-alt" size={20} color="white" />
+        </TouchableOpacity>
       </View>
     </BasePage>
   );
@@ -26,5 +46,20 @@ const styles = StyleSheet.create({
   container: {
     display: 'flex',
     gap: 10,
-  }
+  },
+  button: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1253bc",
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 10,
+  },
+  buttonLabel: {
+    color: "white",
+    fontWeight: "bold",
+  },
 });
